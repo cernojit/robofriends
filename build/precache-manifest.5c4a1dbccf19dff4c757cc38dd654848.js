3487d72e13e411a3026ece805ea7d16013e42c97
cernojit <cernojit@users.noreply.github.com>
2020-03-15 09:43:31 +0100

    added built folder

---
+++ build/precache-manifest.5c4a1dbccf19dff4c757cc38dd654848.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31ceea1a4e1df10f6d39f918e528459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc12c4ca2b1d1ca96522",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "58709eada70f4e63e8d4",
+    "url": "/static/css/main.18f668a2.chunk.css"
+  },
+  {
+    "revision": "fc12c4ca2b1d1ca96522",
+    "url": "/static/js/2.a46dfb62.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a46dfb62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58709eada70f4e63e8d4",
+    "url": "/static/js/main.eb4adea5.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
